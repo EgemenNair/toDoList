676dc171252b7132a55c6a05f0ada5edffb9f533
Dpeche Mode <nairegemen@gmail.com>
2022-08-17 17:16:59 +0300

    Main Structure added to main.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Toast DOM's
+const successToastDOM = document.querySelector("#successToast")
+const errorToastDOM = document.querySelector("#errorToast")
+
+// Header DOM's
+const taskDOM = document.querySelector("#task")
+const addButtonDOM = document.querySelector("#addButton")
+
+// List DOM's
+const ulDOM = document.querySelector("list")
+
+// Event Listeners
+document.addEventListener()
+
+
+// Functions
+function newElement() {
+    return
+}
+
+function removeElement() {
+    return
+}
+
+function checkElement() {
+    return
+}
+
+
+
+
